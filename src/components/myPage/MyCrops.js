a24ef08af929a3ade5e37bbd1f2327e44dbf4659
sujin kim <sujin.wwjd@gmail.com>
2022-07-04 15:32:18 +0900

    작물 등록 오류

--- src/components/myPage/MyCrops.js
+++ src/components/myPage/MyCrops.js
@@ -4,46 +4,47 @@ import { useDispatch, useSelector } from "react-redux";
 import { getCropsListDB } from "../../redux/modules/users";
 import Select from 'react-select'
 import { isDayjs } from "dayjs";
+import { toBeChecked } from "@testing-library/jest-dom/dist/matchers";
 
-const MyCrops = () =>{
+const MyCrops = (props) =>{
     const dispatch = useDispatch();
-    const [crops, setCrops] = useState("")
+    const [selectedCrops, setSelectedCrops] = useState([])
     const cropsData = useSelector((state) => state.users.crops);
     useEffect(() => {
         dispatch(getCropsListDB())
     }, []);
-    const initialCrops = cropsData[0]?.types[0]?.crops
-    console.log(initialCrops)
-    const cropsLabel = cropsData[0]?.types[0]?.crops[0].name
-    const cropsValue = cropsData[0]?.types[0]?.crops[0]?.id
-    // console.log(cropsLabel, cropsValue)  
-
-    const options = [
-      initialCrops!==undefined ? initialCrops.map((v,idx)=>{
-        return (
-    {label: "라벨", value: "밸류"}
-      )
-      }) : null
-    ]
-// { label: "작년", value: 2021 },
-// { label: "재작년", value: 2020 },
-// { label: "재재작년", value: 2019 }]
-
-    console.log(options)
-
+    //console.log(cropsData)
+    // const options = 
+    // cropsData !== undefined ? cropsData.map((crops) => {
+    //   return { label: crops.name, value: crops.id };
+    // }) : null;
+    
+  //  const myCropsId = selectedCrops.filter((option)=>option.value)
+    
+    console.log(selectedCrops)
+    // const putValue = e => {
+    //   setMyCrops(e.value.id)
+    // }
+    //props.setCrops(selectedCrops)
+    //console.log(selectedCrops)
     return(
       <Container>
-        <Select
+        <Select 
         className="react-select"
-        //defaultValue={[options]}
+        defaultValue={[]} //db에서 유저data 불러올 때 다시 수정 필요함
         isMulti 
         name="crops"
-        options={options}
-
+        options=
+          {cropsData !== undefined ? cropsData.map((crops) => {
+            return { label: crops.name, value: crops.id };
+          }) : null}
+        onChange={value=> {setSelectedCrops(
+          ...value)
+          }}
+          
         classNamePrefix="select"
-   
         
-        //onChange={(e) => setCrops(e.target.value)}
+        
         />
       </Container>
     )
