936eec9d5c09f701410c17f1969c76f0aa7a009a
sujin kim <sujin.wwjd@gmail.com>
2022-07-09 11:47:21 +0900

    refreshToken 추가

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+const baseURL = "http://idontcare.shop"
 const token = sessionStorage.getItem("jwtToken");
-
+const refreshToken = sessionStorage.getItem("refreshToken");
 const api = axios.create({
-  baseURL: "http://idontcare.shop",
+  baseURL: baseURL,
   headers: {
     "content-type": "application/json;charset=UTF-8",
     accept: "application/json,",
@@ -13,7 +13,7 @@ const api = axios.create({
 
 // form data용
 const formApi = axios.create({
-  baseURL: "http://idontcare.shop",
+  baseURL: baseURL,
   // "http://3.39.230.66",
   headers: {
     "content-type": "multipart/form-data",
@@ -22,57 +22,22 @@ const formApi = axios.create({
 
 api.interceptors.request.use(function (config) {
   config.headers.common["Authorization"] = `Bearer ${token}`;
+  config.headers.common["RefreshToken"] = `Bearer ${refreshToken}`;
   return config;
-}); //여기까지만 있었다.
-//   function (err) {
-//     return Promise.reject(err);
-//   }
-// );
-
-// api.interceptors.response.use(
-//   function(response){
-//     return response;
-//   },
-//   async function(err){
-//     const {
-//       config,
-//       response: { status },
-//     } = err;
-//     if (status === 401) {
-//       if (err.response.data.message === "TokenExpiredError") {
-//         const originalRequest = config;
-//         const refreshToken = await AsyncStorage.getItem("refreshToken");
-//         //token refresh 요청
-//         const {data} = await axios.post(
-//           'http://localhost:3000/refresh/token',//token refresh api
-//           {
-//             refreshToken,
-//           }
-//         );
-//         //새로운 토큰 저장
-//         const {
-//           accessToken: newAccessToken,
-//           refreshToken: newRefreshToken,
-//         } = data;
-//         await AsyncStorage.multiSet([
-//           ["accessToken",newAccessToken],
-//           ["refreshToken", newRefreshToken],
-//         ]);
-//         api.defaults.headers.common.Authorization = `Baearer ${newAccessToken}`;
-//         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-//         //401로 요청 실패 했었던 요청을 새롭게 accessToken으로 재요청
-//         return axios(originalRequest);
-//         }
-
-//       }
-//       return Promise.reject(err);
-//     }
-// )
+}); 
 
 formApi.interceptors.request.use(function (config) {
   config.headers.common["Authorization"] = `Bearer ${token}`;
+  config.headers.common["RefreshToken"] = `Bearer ${refreshToken}`
   return config;
 });
+api.interceptors.response.use(response=>{
+  if(response.headers.authorization != undefined){
+    console.log("set New Token")
+    sessionStorage.setItem("jwtToken", response.headers.authorization);
+  }
+  return response;
+})
 
 export const apis = {
   //오늘날씨
