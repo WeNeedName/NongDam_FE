065f929458988b75c2a62d20413792daebb80354
sujin kim <sujin.wwjd@gmail.com>
2022-07-18 19:33:39 +0900

    [DELETE] 공백 삭제

--- src/components/workLog/WorkPhoto.js
+++ src/components/workLog/WorkPhoto.js
@@ -1,59 +1,64 @@
-import {React, useState, useEffect} from 'react'
+import { React, useState, useEffect } from "react";
 import styled from "styled-components";
-import {useSelector, useDispatch} from "react-redux"
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-const WorkPhoto =({setImages}) => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [imageSrc, setImageSrc] = useState("");
+const WorkPhoto = ({ setImages }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [imageSrc, setImageSrc] = useState("");
 
-    const  encodeFileToBase64 = (fileBlob) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(fileBlob);
-      return new Promise((resolve) => {
-        reader.onload= () => {
-          setImageSrc(reader.result);
-          resolve();
-        };
-      });
-    };
+  const encodeFileToBase64 = (fileBlob) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(fileBlob);
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setImageSrc(reader.result);
+        resolve();
+      };
+    });
+  };
 
-      
-    const onChangeFile = (e) => {
-      //console.log(e.target.files[0])
-        setImages(e.target.files[0])
-      } 
-     
-    
-    return(
-        <ImageContentWrap>
-        <SmallTitle>사진(선택사항)</SmallTitle>
-        <CategoryBigWrap>
-            {/* <p className="upload">이미지 미리보기</p> */}
-              <InputBox type="file" 
-              onChange ={(e) => 
-              {{encodeFileToBase64(e.target.files[0])}
-               {onChangeFile(e)}
-              }} />
-            <div className="preview">
-            {imageSrc && <ImagePreview src={imageSrc} alt="preview-img" />}  
-            </div>
-            </CategoryBigWrap>
-    </ImageContentWrap>  
-    )
-}
+  const onChangeFile = (e) => {
+    //console.log(e.target.files[0])
+    setImages(e.target.files[0]);
+  };
+  console.log(imageSrc);
+
+  return (
+    <ImageContentWrap>
+      <SmallTitle>사진(선택사항)</SmallTitle>
+      <CategoryBigWrap>
+        {/* <p className="upload">이미지 미리보기</p> */}
+        <InputBox
+          type="file"
+          onChange={(e) => {
+            {
+              encodeFileToBase64(e.target.files[0]);
+            }
+            {
+              onChangeFile(e);
+            }
+          }}
+        />
+        <div className="preview">
+          {imageSrc && <ImagePreview src={imageSrc} alt="preview-img" />}
+        </div>
+      </CategoryBigWrap>
+    </ImageContentWrap>
+  );
+};
 const ImageContentWrap = styled.div`
-width: 200px;
-height: 80px;
-background-color: #fff;
-padding-left : 30px;
-padding-top : 30px;
-`
+  width: 200px;
+  height: 80px;
+  background-color: #fff;
+  padding-left: 30px;
+  padding-top: 30px;
+`;
 const SmallTitle = styled.label`
-font-size: 18px;
-font-weight: 700;
-`
+  font-size: 18px;
+  font-weight: 700;
+`;
 
 const CategoryBigWrap = styled.div`
   width: 60%
@@ -62,14 +67,11 @@ const CategoryBigWrap = styled.div`
   margin-top: 10px;
 `;
 
-const InputBox = styled.input``
+const InputBox = styled.input``;
 
 const ImagePreview = styled.img`
   width: 100px;
   height: 100px;
-`
-
-
-
+`;
 
 export default WorkPhoto;
