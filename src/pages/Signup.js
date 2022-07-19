871da0cd1dfb4dcfd869c812c03dbaba0bdfbdaf
sujin kim <sujin.wwjd@gmail.com>
2022-07-19 16:33:44 +0900

    [STYLE] 회원가입 에러메시지 수정

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -67,11 +67,13 @@ const Signup = () => {
       nickname: userNickname,
       name: userName,
     };
-    dispatch(signUpDB(userInfo)).then(res => {
-      window.alert("회원가입이 완료되었습니다. 입력하신 이메일에서 메일 인증후 이용 가능합니다.")
-      navigate("/login")
-    })
-      
+    dispatch(signUpDB(userInfo)).then((res) => {
+      window.alert(
+        "회원가입이 완료되었습니다. 입력하신 이메일에서 메일 인증후 이용 가능합니다."
+      );
+      navigate("/login");
+    });
+
     //     if(userNickname?.response?.status === 400) {
     //         setNicknameErr(true);
     //         window.alert(userNickname?.response?.data);
@@ -79,7 +81,6 @@ const Signup = () => {
     //         setNicknameErr(true);
     //     }else setNicknameErr(false);
   };
-
   //console.log(userId, pw)
 
   const handleSubmit = async (e) => {
@@ -91,19 +92,27 @@ const Signup = () => {
   useEffect(() => {
     emailRef.current.focus();
   }, []);
+
   return (
-    <>
-      <h1>회원가입</h1>
-      {/* <form onSubmit={handleSubmit}> */}
-      <InputBoxes>
-        <EmailWrap>
+    <Container>
+      <TopWrap>
+        <p className="title">Nongdam</p>
+        <p className="slogan"> 농장을 한 눈에 담다, 농담!</p>
+        <p className="desc">
+          행복한 농사 생활을 위한 농담 <br />내 농장에 필요한 모든 걸 한 눈에
+          보세요.
+        </p>
+      </TopWrap>
+
+      <InputBoxesWrap>
+        <InputWrap>
           <LabelEmail>이메일</LabelEmail>
-          <input
+          <InputBox
             ref={emailRef}
             className="inputId"
             type="email"
             //userIdErr={userIdErr}
-            placeholder="이메일을 입력해주세요"
+            // placeholder="이메일을 입력해주세요"
             onChange={(e) => {
               onChangeUserId(e);
             }}
@@ -122,142 +131,256 @@ const Signup = () => {
                                 <option value="icloud.com">icloud.com</option>
                                 <option value="direct">직접입력</option>
                         </SelectEM> */}
-        </EmailWrap>
+        </InputWrap>
 
-        {userIdErr && <EmailErr>이메일 형식이 올바르지 않다.</EmailErr>}
+        {userIdErr && <EmailErr>이메일 형식이 올바르지 않습니다.</EmailErr>}
+        {!userIdErr && email !== "" && (
+          <EmailOk>사용 가능한 이메일 형식 입니다.</EmailOk>
+        )}
 
-        <PWWrap>
-          <LabelP pwErr={pwErr}>PW</LabelP>
+        <InputWrap>
+          <LabelPw pwErr={pwErr}>비밀번호</LabelPw>
           {/* <Info>영문, 숫자 포함한 8자 이상의 비밀번호 입력해주세요</Info> */}
-          <InputPw
+          <InputBox
             pwErr={pwErr}
             onChange={(e) => {
               onChangePw(e);
             }}
-            placeholder="비밀번호(영문, 숫자 포함 8자 이상)"
+            // placeholder="비밀번호(영문, 숫자 포함 8자 이상)"
             type="password"
           />
+
+          {pw === "" && (
+            <InfoPw> 최소 8자, 하나 이상의 숫자를 포함해주세요.</InfoPw>
+          )}
           {pwErr && (
             <PwErr>비밀번호는 영문, 숫자 포함 8자 이상이여야 합니다.</PwErr>
           )}
-        </PWWrap>
+          {pw !== "" && !pwErr && <PwOk>올바른 비밀번호 입니다.</PwOk>}
+        </InputWrap>
 
-        <PwCheckWrap>
-          <LabelPC>PW 체크</LabelPC>
-          <InputPwCheck
+        <InputWrap>
+          <LabelPC>비밀번호 확인</LabelPC>
+          <InputBox
             pwCheckErr={pwCheckErr}
             onChange={(e) => {
               onChangePwCheck(e);
             }}
-            placeholder="비밀번호 확인"
+            //placeholder="비밀번호를 한 번 더 입력해주세요"
             type="password"
           />
-          {!pwErr && pwCheckErr && <PwErr>비밀번호가 일치하지 않습니다.</PwErr>}
-        </PwCheckWrap>
 
-        <NameWrap>
-          <LabelN>이름</LabelN>
-          <InputName
+          {pwCheck === "" && <InfoPc>비밀번호를 한 번 더 입력해주세요.</InfoPc>}
+          {pwCheck !== "" && !pwErr && pwCheckErr && (
+            <PwCheckErr>비밀번호가 일치하지 않습니다.</PwCheckErr>
+          )}
+          {pwCheck !== "" && !pwErr && !pwCheckErr && (
+            <PwCheckOk>비밀번호가 일치합니다.</PwCheckOk>
+          )}
+        </InputWrap>
+
+        <InputWrap>
+          <LabelName>이름</LabelName>
+          <InputBox
             value={userName || ""}
-            placeholder="이름"
+            // placeholder="이름"
             onChange={(e) => setUserName(e.target.value)}
           />
-        </NameWrap>
-        <NicknameWrap>
-          <LabelNN>닉네임</LabelNN>
-          <InputNickname
-            placeholder="닉네임"
+        </InputWrap>
+
+        <InputWrap>
+          <LabelNickname>닉네임</LabelNickname>
+          <InputBox
+            // placeholder="닉네임"
             value={userNickname || ""}
             onChange={(e) => setUserNickname(e.target.value)}
           />
-        </NicknameWrap>
-      </InputBoxes>
-
-      <SignUpBtn
-        type="submit"
-        onClick={() => {
-          signUp(email, pw, userName, userNickname);
-        }}
-        disabled={
-          !email || !pw || !pwCheck || userIdErr || pwCheckErr ? true : false
-        }
-      >
-        회원가입
-      </SignUpBtn>
-      {/* </form> */}
-    </>
+        </InputWrap>
+      </InputBoxesWrap>
+      <BtnWrap>
+        <SignUpBtn
+          type="submit"
+          onClick={() => {
+            signUp(email, pw, userName, userNickname);
+          }}
+          disabled={
+            !email || !pw || !pwCheck || userIdErr || pwCheckErr ? true : false
+          }
+        >
+          회원가입
+        </SignUpBtn>
+      </BtnWrap>
+    </Container>
   );
 };
 
-const InputBoxes = styled.div`
+const Container = styled.div`
+  margin-top: 60px;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-item: center;
+  position: relative;
+`;
+
+const TopWrap = styled.div`
+  .title {
+    font-weight: 700;
+    font-size: 30px;
+    color: #318f27;
+    margin-bottom: 40px;
+  }
+  .slogan {
+    font-weight: 700;
+    font-size: 20px;
+  }
+  .desc {
+    font-size: 11px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    color: #666666;
+  }
+`;
+
+const InputBoxesWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   margin: 10px;
 `;
-//이메일 스타일드 컴포넌트
-const EmailWrap = styled.div`
+
+const InputWrap = styled.div`
   display: flex;
-  margin: 10px;
+  flex-direction: column;
+  margin-top: 15px;
+  margin-bottom: 3px;
 `;
 
 const LabelEmail = styled.label`
-  color: rgb(
-    ${(props) => (props.nickNameErr ? "255, 119, 119" : "41, 41, 41")}
-  );
-`;
-const EmailErr = styled.div``;
-
-//패스워드 스타일드컴포넌트
-const PWWrap = styled.div`
-  display: flex;
-  margin: 10px;
+  margin-right: 220px;
+  margin-bottom: 5px;
+  color: rgb(${(props) => (props.EmailErr ? "255, 119, 119" : "41, 41, 41")});
 `;
 
-const LabelP = styled.label`
-  color: rgb(
-    ${(props) => (props.nickNameErr ? "255, 119, 119" : "41, 41, 41")}
-  );
+const InputBox = styled.input`
+  width: 240px;
+  height: 30px;
+  border: 1px solid #999999;
+  border-radius: 5px;
+  padding: 5px;
 `;
-const Info = styled.p``;
-const InputPw = styled.input``;
-const PwErr = styled.p``;
 
-//패스워드확인 스타일드 컴포넌트
-const PwCheckWrap = styled.div`
-  display: flex;
-  margin: 10px;
+const EmailErr = styled.div`
+  margin-right: 105px;
+
+  font-size: 11px;
+  color: #ec0000;
 `;
+
+const EmailOk = styled.div`
+  margin-right: 110px;
+
+  font-size: 11px;
+  color: #0a9c19;
+`;
+
+const LabelPw = styled.label`
+  margin-right: 210px;
+  margin-bottom: 5px;
+  color: rgb(${(props) => (props.PwErr ? "255, 119, 119" : "41, 41, 41")});
+`;
+
+const InfoPw = styled.div`
+  margin-right: 60px;
+  margin-top: 3px;
+  font-size: 11px;
+  font-color: #666666;
+`;
+
+const PwErr = styled.div`
+  margin-right: 30px;
+  margin-top: 3px;
+  font-size: 11px;
+  color: #ec0000;
+`;
+
+const PwOk = styled.div`
+  margin-right: 145px;
+  margin-top: 3px;
+  font-size: 11px;
+  color: #0a9c19;
+`;
+
 const LabelPC = styled.label`
-  color: rgb(
-    ${(props) => (props.nickNameErr ? "255, 119, 119" : "41, 41, 41")}
-  );
+  margin-right: 180px;
+  margin-bottom: 5px;
+  color: rgb(${(props) => (props.PwCheckErr ? "255, 119, 119" : "41, 41, 41")});
 `;
-const InputPwCheck = styled.input``;
 
-//이름 스타일드 컴포넌트
-const NameWrap = styled.div`
+const PwCheckErr = styled.div`
+  margin-right: 120px;
+  margin-top: 3px;
+  font-size: 11px;
+  color: #ec0000;
+`;
+
+const InfoPc = styled.div`
+  margin-right: 100px;
+  margin-top: 3px;
+  font-size: 11px;
+  font-color: #666666;
+`;
+
+const PwCheckOk = styled.div`
+  margin-right: 150px;
+  margin-top: 3px;
+  font-size: 11px;
+  color: #0a9c19;
+`;
+
+const LabelName = styled.label`
+  margin-right: 230px;
+  margin-bottom: 5px;
+  // color: rgb(
+  //   ${(props) => (props.NameErr ? "255, 119, 119" : "41, 41, 41")}
+  // );
+`;
+
+const LabelNickname = styled.label`
+  margin-right: 220px;
+  margin-bottom: 5px;
+  // color: rgb(
+  //   ${(props) => (props.NicknameErr ? "255, 119, 119" : "41, 41, 41")}
+  // );
+`;
+
+const BtnWrap = styled.div`
   display: flex;
-  margin: 10px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
-const LabelN = styled.label`
-  color: rgb(
-    ${(props) => (props.nickNameErr ? "255, 119, 119" : "41, 41, 41")}
-  );
-`;
-const InputName = styled.input``;
 
-//닉네임 스타일드 컴포넌트
-const NicknameWrap = styled.div`
-  display: flex;
+const SignUpBtn = styled.button`
+  width: 253px;
+  height: 40px;
+  flex-direction: center;
+  align-text: center;
+  justify-content: center;
+  text-align: center;
+  padding: 4px 13px;
+  background-color: #22631c;
+  color: white;
+  border-radius: 8px;
+  border: none;
+  size: 11px;
   margin: 10px;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
 `;
-const LabelNN = styled.label`
-  color: rgb(
-    ${(props) => (props.nickNameErr ? "255, 119, 119" : "41, 41, 41")}
-  );
-`;
-const InputNickname = styled.input``;
-
-const SignUpBtn = styled.button``;
 
 export default Signup;
