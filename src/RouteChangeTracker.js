22e9ba230cd75f35f621df903cbb25dc4527929e
SonshineLiveinEarth <yoonjooson8@gmail.com>
2022-07-28 14:20:30 +0900

    [FEAT] GA 추적코드 적용

---
+++ src/RouteChangeTracker.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga";
+
+const RouteChangeTracker = () => {
+  const location = useLocation();
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!window.location.href.includes("localhost")) {
+      // 환경 변수 사용
+      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID);
+    }
+    setInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.pageview(location.pathname + location.search);
+    }
+  }, [initialized, location]);
+};
+
+export default RouteChangeTracker;
